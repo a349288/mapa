7883265324544f662ee0dfc1f4b7dd8d8318de0c
a349288 <102832151+a349288@users.noreply.github.com>
2022-04-01 15:01:39 -0700

    Update app_map.py

--- app_map.py
+++ app_map.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import pandas as ps 
 
-#df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/a349288/mapa/main/data.csv")
