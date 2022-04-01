30bea99454eae650c694979467f82b33c0d80c19
a349288 <102832151+a349288@users.noreply.github.com>
2022-04-01 15:04:46 -0700

    Update app_map.py

--- app_map.py
+++ app_map.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as ps 
 
-df = pd.read_csv("https://raw.githubusercontent.com/a349288/mapa/main/data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
 st.write(df)
 st.map(df)
